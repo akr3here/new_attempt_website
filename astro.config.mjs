c5d52c515c30a88c486fe9738e7a614cbb3206e1
akr3here <alokrhymer3@outlook.com>
2026-06-23 23:33:31 +0530

    Revert "Add Pagefind search"
    This reverts commit 440ee3dc27d9712e4739f6cb1480d300a14f459c.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,14 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
-import pagefind from 'astro-pagefind';
 
 export default defineConfig({
+  // ✏️ Change this to your actual domain before deploying
   site: 'https://alokray.com',
   integrations: [
     mdx(),
     sitemap(),
     tailwind({ applyBaseStyles: false }),
-    pagefind(),
   ],
   markdown: {
     shikiConfig: {
